5f04fb6435b82bec1dc93c2a3e0bbfdc8f97b293
Melissa <melissakheying@gmail.com>
2020-08-11 11:52:59 -0500

    Place props in Table Data

---
+++ src/components/StudentExams/Table/TStudent/index.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+
+
+export const TStudent = ({firstName, lastName, score}) => {
+
+
+  return (
+    <Fragment>
+      <td>{firstName}</td>
+      <td>{lastName}</td>
+      <td>{score}</td>
+    </Fragment>
+  )
+}
+
+TStudent.propTypes = {
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+  score: PropTypes.number,
+}
