c7eb1ba777244842299fb8596572d69cc21397eb
Melissa <melissakheying@gmail.com>
2020-08-11 11:11:05 -0500

    :construction: Add Table HTML

--- src/components/StudentExams/Table/index.js
+++ src/components/StudentExams/Table/index.js
@@ -1,10 +1,46 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export const Table = () => {
+export const Table = ( {student: {firstName, lastName, score}}) => {
+
+
+
+const renderTableRow = () => {
+
+
+}
+
 
 
 
   return (
-    <p>Ima table</p>
+<table>
+  <thead>
+    <tr>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Score</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>{firstName}</td>
+      <td>{lastName}</td>
+      <td>{score}</td>
+    </tr>
+  </tbody>
+  <tfoot>
+    <tr>
+      <td>Average</td>
+      <td>$AveNUM</td>
+    </tr>
+  </tfoot>
+</table>
   )
+}
+
+
+Table.propTypes = {
+  student: PropTypes.object,
+
 }
