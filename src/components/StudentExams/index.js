4112676d337d136a1a36627700cf3c2955748fcb
Melissa <melissakheying@gmail.com>
2020-08-11 09:49:58 -0500

    :construction: Set up components

---
+++ src/components/StudentExams/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Table} from './Table'
+import { Form } from './Form'
+
+export const StudentExams = () => {
+
+
+
+
+  return (
+    <main>
+      <h1>Student Exams</h1>
+    <Table />
+    <Form />
+    </main>
+  )
+}
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export {StudentExams} from './StudentExams'
