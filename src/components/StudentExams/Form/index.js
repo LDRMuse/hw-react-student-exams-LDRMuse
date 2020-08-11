dbc10b1e2b2d722c2f502cd9d20ed0f092441c6d
Melissa <melissakheying@gmail.com>
2020-08-11 11:11:31 -0500

    :construction: Add input HTML

--- src/components/StudentExams/Form/index.js
+++ src/components/StudentExams/Form/index.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export const Form = () => {
+export const Form = ({handler}) => {
 
 
 
 
   return (
-    <p>Ima form</p>
+    <form onClick={handler}>
+
+  <label htmlFor="fname" placeholder="First Name"></label>
+  <input type="text" id="fname" name="fname"/>
+
+  <label htmlFor="lname" placeholder="Last Name"></label>
+  <input type="text" id="lname" name="lname"/>
+
+  <label htmlFor="score" placeholder="Score"></label>
+  <input type="number" id="score" />
+  <button>Add</button>
+</form>
   )
+}
+
+Form.propTypes = {
+handler: PropTypes.func,
 }
